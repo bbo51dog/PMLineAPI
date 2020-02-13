7da937c4dd3179c7832f2c06bb9aa33db237102a
bbo51dog <bbo51@icloud.com>
2020-02-13 22:04:49 +0900

    Bot interface

--- src/bbo51dog/pmline/bot/Bot.php
+++ src/bbo51dog/pmline/bot/Bot.php
@@ -2,6 +2,15 @@
 
 namespace bbo51dog\pmline\bot;
 
+use bbo51dog\pmline\message\MessageList;
+
 interface Bot{
 
+    public function sendPush(MessageList $list): void;
+    
+    public function sendMulticast(MessageList $list): void;
+    
+    public function sendNarrowcast(MessageList $list): void;
+    
+    public function sendBroadcast(MessageList $list): void;
 }
