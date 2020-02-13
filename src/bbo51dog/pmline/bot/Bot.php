e96b41b9b6a6e98aaae851011d60c90ec7b12b6a
bbo51dog <bbo51@icloud.com>
2020-02-13 23:26:15 +0900

    PHPDoc

--- src/bbo51dog/pmline/bot/Bot.php
+++ src/bbo51dog/pmline/bot/Bot.php
@@ -6,11 +6,35 @@ use bbo51dog\pmline\message\MessageList;
 
 interface Bot{
 
+    /**
+     * Send push message
+     * You cannot send if you use free plan or basic plan
+     *
+     * @param MessageList $list
+     * @param string $to
+     */
     public function sendPush(MessageList $list, string $to): void;
     
+    /**
+     * Send multicast message
+     * You cannot send if you use free plan or basic plan
+     *
+     * @param MessageList $list
+     * @param array $to
+     */
     public function sendMulticast(MessageList $list, array $to): void;
     
+    /**
+     * Send narrowcast message
+     *
+     * @param MessageList $list
+     */
     public function sendNarrowcast(MessageList $list): void;
     
+    /**
+     * Send broadcast message
+     *
+     * @param MessageList $list
+     */
     public function sendBroadcast(MessageList $list): void;
 }
